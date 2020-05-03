0986de979ae22cb492df4e5974ae434854e6d61b
Tank361 <60121580+Tank361@users.noreply.github.com>
2020-05-03 18:27:38 +0800

    0503 筆記本更新

--- app/src/main/java/com/example/final_projects/Note.java
+++ app/src/main/java/com/example/final_projects/Note.java
@@ -4,56 +4,108 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Note extends AppCompatActivity {
     private EditText inputText;
     private ListView listInput;
-    private ArrayAdapter<String> adapter;
-    private ArrayList<String> items;
-
+    private NoteDBHelper helper;
+    private Cursor cursor;
+    private SimpleCursorAdapter cursorAdapter;
+    private List<String> option;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        initDB();
+        initView();
+    }
+
+    private void initDB(){
+        helper = new NoteDBHelper(getApplicationContext());
+        cursor = helper.select();
+        listInput = (ListView)findViewById(R.id.listInputText);
+        cursorAdapter = new SimpleCursorAdapter(this,
+                R.layout.note_adapter, cursor,
+                new String[]{"item_text"},
+                new int[]{R.id.text},
+                SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
+        );
+    }
+
+    private void initView(){
+        option = new ArrayList<>();
+        option.add(getApplicationContext().getString(R.string.modify));
+        option.add(getString(R.string.delete));
         inputText = (EditText)findViewById(R.id.inputText);
         listInput = (ListView)findViewById(R.id.listInputText);
-        items = new ArrayList<>();
-        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,items);
-        listInput.setAdapter(adapter);
+        listInput.setAdapter(cursorAdapter);
         listInput.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> arg0, View view, int position, long id) {
                 final int pos = position;
+                cursor.moveToPosition(1);
                 new AlertDialog.Builder(Note.this)
-                        .setTitle("刪除列")
-                        .setMessage("你確定要刪除？")
-                        .setPositiveButton("是", new DialogInterface.OnClickListener() {
-
+                        .setItems(option.toArray(new String[option.size()]), new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                items.remove(pos);
-                                listInput.setAdapter(adapter);
+                                switch (which) {
+                                    case 0://modify
+                                        final View item = LayoutInflater.from(Note.this).inflate(R.layout.note_alertdialog, null);
+                                        final EditText editText = (EditText) item.findViewById(R.id.edittext);
+                                        editText.setText(cursor.getString(1));
+                                        new AlertDialog.Builder(Note.this)
+                                                .setTitle("修改數值")
+                                                .setView(item)
+                                                .setPositiveButton("修改", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        helper.update(cursor.getInt(0), editText.getText().toString());
+                                                        cursor.requery();
+                                                        cursorAdapter.notifyDataSetChanged();
+                                                    }
+                                                })
+                                                .show();
+                                        break;
+                                    case 1://delete
+                                        new AlertDialog.Builder(Note.this)
+                                                .setTitle("刪除列")
+                                                .setMessage("你確定要刪除？")
+                                                .setPositiveButton("是", new DialogInterface.OnClickListener() {
+
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        helper.delete(cursor.getInt(0));
+                                                        cursor.requery();
+                                                        cursorAdapter.notifyDataSetChanged();
+                                                    }
+                                                })
+                                                .setNegativeButton("否", new DialogInterface.OnClickListener() {
+
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+
+                                                    }
+                                                })
+                                                .show();
+                                        break;
+                                }
+
                             }
-                        })
-                        .setNegativeButton("否", new DialogInterface.OnClickListener() {
-
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                            }
-                        })
-                        .show();
-
+                        }).show();
                 return false;
             }
 
@@ -70,14 +122,15 @@ public class Note extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case Menu.FIRST:
+            case Menu.FIRST://add new item
                 if(!inputText.getText().toString().equals("")){
-                    items.add(inputText.getText().toString());
-                    listInput.setAdapter(adapter);
+                    helper.insert(inputText.getText().toString());
+                    cursor.requery();
+                    cursorAdapter.notifyDataSetChanged();
                     inputText.setText("");
                 }
                 break;
-            case Menu.FIRST + 1:
+            case Menu.FIRST + 1://exit app
                 new AlertDialog.Builder(Note.this)
                         .setTitle("離開此程式")
                         .setMessage("你確定要離開？")
