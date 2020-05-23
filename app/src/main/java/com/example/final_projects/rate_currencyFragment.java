231d7a22b40bdc4d63f6e746fe7f092ff002d7b9
Tank361 <60121580+Tank361@users.noreply.github.com>
2020-05-23 18:21:39 +0800

    0514 匯率更新並連上資料庫資料(能自動更新以及能夠計算)

--- app/src/main/java/com/example/final_projects/rate_currencyFragment.java
+++ app/src/main/java/com/example/final_projects/rate_currencyFragment.java
@@ -5,14 +5,25 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class rate_currencyFragment extends Fragment {
-
-
+    private View mainView;
+    double USDcash,USDspot,JPYcash,JPYspot,EURcash,EURspot,CNYcash,CNYspot,AUDcash,AUDspot;
+    private TextView txv3,txv4,txv5,txv6,txv7,txv8,txv9,txv10,txv11,txv12;
     public rate_currencyFragment() {
         // Required empty public constructor
     }
@@ -22,7 +33,111 @@ public class rate_currencyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.rate_currency, container, false);
-    }
 
+        sendRequestWithHttpURLConnection();
+
+
+
+        mainView=inflater.inflate(R.layout.rate_currency, container, false);
+        txv3 = mainView.findViewById(R.id.textView3);
+        txv4 = mainView.findViewById(R.id.textView4);
+        txv5 = mainView.findViewById(R.id.textView5);
+        txv6 = mainView.findViewById(R.id.textView6);
+        txv7 = mainView.findViewById(R.id.textView7);
+        txv8 = mainView.findViewById(R.id.textView8);
+        txv9 = mainView.findViewById(R.id.textView9);
+        txv10 = mainView.findViewById(R.id.textView10);
+        txv11 = mainView.findViewById(R.id.textView11);
+        txv12 = mainView.findViewById(R.id.textView12);
+
+
+        txv4.setText(String.valueOf(USDspot));
+        txv6.setText(String.valueOf(JPYspot));
+        txv8.setText(String.valueOf(EURspot));
+        txv10.setText(String.valueOf(CNYspot));
+        txv12.setText(String.valueOf(AUDspot));
+
+
+        txv3.setText(String.valueOf(USDcash));
+        txv5.setText(String.valueOf(JPYcash));
+        txv7.setText(String.valueOf(EURcash));
+        txv9.setText(String.valueOf(CNYcash));
+        txv11.setText(String.valueOf(AUDcash));
+        return mainView;
+    }
+    private void sendRequestWithHttpURLConnection() {
+        //开启线程发起网络请求
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader ;
+                try {
+                    //获取HttpRULConnection实例
+                    URL url = new URL("http://163.17.9.130/currency.php");
+                    connection = (HttpURLConnection) url.openConnection();
+                    //设置请求方法和自由定制
+                    connection.setUseCaches(false);
+                    connection.setRequestMethod("POST");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+                    connection.setRequestProperty("Content-Type","application/json;charset=UTF-8");
+                    connection.connect();
+
+
+
+                    InputStream in = connection.getInputStream();
+                    //对输入流进行读取
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    JSONArray array = new JSONArray(reader.readLine());
+
+                    //////////美金
+                    JSONObject jsonObject0 = array.getJSONObject(0);
+                    Double USDcashRateBuying = jsonObject0.getDouble("cashRateBuying");
+                    Double USDspotRateBuying = jsonObject0.getDouble("spotRateBuying");
+                    USDcash =USDcashRateBuying;
+                    USDspot =USDspotRateBuying;
+
+                    ////////日幣
+                    JSONObject jsonObject7 = array.getJSONObject(7);
+                    Double JPYcashRateBuying = jsonObject7.getDouble("cashRateBuying");
+                    Double JPYspotRateBuying = jsonObject7.getDouble("spotRateBuying");
+                    JPYcash =JPYcashRateBuying;
+                    JPYspot =JPYspotRateBuying;
+
+                    ////////歐元
+                    JSONObject jsonObject14 = array.getJSONObject(14);
+                    Double EURcashRateBuying = jsonObject14.getDouble("cashRateBuying");
+                    Double EURspotRateBuying = jsonObject14.getDouble("spotRateBuying");
+                    EURcash =EURcashRateBuying;
+                    EURspot =EURspotRateBuying;
+
+                    ////////人民幣
+                    JSONObject jsonObject18 = array.getJSONObject(18);
+                    Double CNYcashRateBuying = jsonObject18.getDouble("cashRateBuying");
+                    Double CNYspotRateBuying = jsonObject18.getDouble("spotRateBuying");
+                    CNYcash =CNYcashRateBuying;
+                    CNYspot =CNYspotRateBuying;
+
+                    ////////澳幣
+                    JSONObject jsonObject3 = array.getJSONObject(3);
+                    Double AUDcashRateBuying = jsonObject3.getDouble("cashRateBuying");
+                    Double AUDspotRateBuying = jsonObject3.getDouble("spotRateBuying");
+                    AUDcash =AUDcashRateBuying;
+                    AUDspot =AUDspotRateBuying;
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
 }
--- app/src/main/java/com/example/final_projects/rate_exchangeRateFragment.java
+++ app/src/main/java/com/example/final_projects/rate_exchangeRateFragment.java
@@ -12,6 +12,14 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DecimalFormat;
 
 
@@ -24,10 +32,13 @@ public class rate_exchangeRateFragment extends Fragment {
     private TextView txv14,txv16,txv18,txv20,txv22;
     private RadioGroup rg;
     private RadioButton spot, cash;
+    double USDcash,USDspot,JPYcash,JPYspot,EURcash,EURspot,CNYcash,CNYspot,AUDcash,AUDspot,eqlTWD;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        sendRequestWithHttpURLConnection();
 
         mainView =  inflater.inflate(R.layout.rate_exchange_rate, container, false);
         Button exchange = mainView.findViewById(R.id.exchange);
@@ -69,43 +80,74 @@ public class rate_exchangeRateFragment extends Fragment {
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             switch (buttonView.getId()) {
                 case R.id.spot:
-                    txv14.setText("30.35");
-                    txv16.setText("0.2819");
-                    txv18.setText("34.11");
-                    txv20.setText("4.381");
-                    txv22.setText("21.28");
-                    etNum2.setHint("30.35");
-                    etNum3.setHint("0.2819");
-                    etNum4.setHint("34.11");
-                    etNum5.setHint("4.381");
-                    etNum6.setHint("21.28");
+                    txv14.setText(String.valueOf(USDcash));
+                    txv16.setText(String.valueOf(JPYcash));
+                    txv18.setText(String.valueOf(EURcash));
+                    txv20.setText(String.valueOf(CNYcash));
+                    txv22.setText(String.valueOf(AUDcash));
+                    etNum2.setHint(String.valueOf(USDcash));
+                    etNum3.setHint(String.valueOf(JPYcash));
+                    etNum4.setHint(String.valueOf(EURcash));
+                    etNum5.setHint(String.valueOf(CNYcash));
+                    etNum6.setHint(String.valueOf(AUDcash));
                     break;
                 case R.id.cash:
-                    txv14.setText("30.13");
-                    txv16.setText("0.2804");
-                    txv18.setText("33.79");
-                    txv20.setText("4.341");
-                    txv22.setText("21");
-                    etNum2.setHint("30.13");
-                    etNum3.setHint("0.2804");
-                    etNum4.setHint("33.79");
-                    etNum5.setHint("4.341");
-                    etNum6.setHint("21");
+                    txv14.setText(String.valueOf(USDspot));
+                    txv16.setText(String.valueOf(JPYspot));
+                    txv18.setText(String.valueOf(EURspot));
+                    txv20.setText(String.valueOf(CNYspot));
+                    txv22.setText(String.valueOf(AUDspot));
+
+                    etNum2.setHint(String.valueOf(USDspot));
+                    etNum3.setHint(String.valueOf(JPYspot));
+                    etNum4.setHint(String.valueOf(EURspot));
+                    etNum5.setHint(String.valueOf(CNYspot));
+                    etNum6.setHint(String.valueOf(AUDspot));
                     break;
             }
         }
     };
-    ;
 
 
     public void exchange() {
         DecimalFormat mDecimalFormat = new DecimalFormat("#,###.##");
-        double TWD = Double.parseDouble(etNum1.getText().toString());
-        double USD = 30.13;
-        double JPY = 0.2804;
-        double EUR = 33.79;
-        double CNY = 4.341;
-        double AUD = 21;
+        if (etNum1.getText().toString().equals("")){
+            eqlTWD=0.0;
+        }
+        else{
+            eqlTWD=Double.parseDouble(etNum1.getText().toString());
+        }
+        double TWD = eqlTWD;
+        double USD = USDspot;
+        double JPY = JPYspot;
+        double EUR = EURspot;
+        double CNY = CNYspot;
+        double AUD = AUDspot;
+        double USDtotal = TWD/USD;
+        double JPYtotal = TWD/JPY;
+        double EURtotal = TWD/EUR;
+        double CNYtotal = TWD/CNY;
+        double AUDtotal = TWD/AUD;
+        etNum2.setText(mDecimalFormat.format(USDtotal));
+        etNum3.setText(mDecimalFormat.format(JPYtotal));
+        etNum4.setText(mDecimalFormat.format(EURtotal));
+        etNum5.setText(mDecimalFormat.format(CNYtotal));
+        etNum6.setText(mDecimalFormat.format(AUDtotal));
+    }
+    public void exchange2() {
+        DecimalFormat mDecimalFormat = new DecimalFormat("#,###.##");
+        if (etNum1.getText().toString().equals("")){
+            eqlTWD=0.0;
+        }
+        else{
+            eqlTWD=Double.parseDouble(etNum1.getText().toString());
+        }
+        double TWD = eqlTWD;
+        double USD = USDcash;
+        double JPY = JPYcash;
+        double EUR = EURcash;
+        double CNY = CNYcash;
+        double AUD = AUDcash;
         double USDtotal = TWD*USD;
         double JPYtotal = TWD*JPY;
         double EURtotal = TWD*EUR;
@@ -117,24 +159,81 @@ public class rate_exchangeRateFragment extends Fragment {
         etNum5.setText(mDecimalFormat.format(CNYtotal));
         etNum6.setText(mDecimalFormat.format(AUDtotal));
     }
-    public void exchange2() {
-        DecimalFormat mDecimalFormat = new DecimalFormat("#,###.##");
-        double TWD = Double.parseDouble(etNum1.getText().toString());
-        double USD = 30.35;
-        double JPY = 0.2819;
-        double EUR = 34.11;
-        double CNY = 4.381;
-        double AUD = 21.28;
-        double USDtotal = TWD*USD;
-        double JPYtotal = TWD*JPY;
-        double EURtotal = TWD*EUR;
-        double CNYtotal = TWD*CNY;
-        double AUDtotal = TWD*AUD;
-        etNum2.setText(mDecimalFormat.format(USDtotal));
-        etNum3.setText(mDecimalFormat.format(JPYtotal));
-        etNum4.setText(mDecimalFormat.format(EURtotal));
-        etNum5.setText(mDecimalFormat.format(CNYtotal));
-        etNum6.setText(mDecimalFormat.format(AUDtotal));
+
+    private void sendRequestWithHttpURLConnection() {
+        //开启线程发起网络请求
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader ;
+                try {
+                    //获取HttpRULConnection实例
+                    URL url = new URL("http://163.17.9.130/currency.php");
+                    connection = (HttpURLConnection) url.openConnection();
+                    //设置请求方法和自由定制
+                    connection.setUseCaches(false);
+                    connection.setRequestMethod("POST");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+                    connection.setRequestProperty("Content-Type","application/json;charset=UTF-8");
+                    connection.connect();
+
+
+
+                    InputStream in = connection.getInputStream();
+                    //对输入流进行读取
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    JSONArray array = new JSONArray(reader.readLine());
+
+                    //////////美金
+                    JSONObject jsonObject0 = array.getJSONObject(0);
+                    Double USDcashRateBuying = jsonObject0.getDouble("cashRateBuying");
+                    Double USDspotRateBuying = jsonObject0.getDouble("spotRateBuying");
+                    USDcash =USDcashRateBuying;
+                    USDspot =USDspotRateBuying;
+
+                    ////////日幣
+                    JSONObject jsonObject7 = array.getJSONObject(7);
+                    Double JPYcashRateBuying = jsonObject7.getDouble("cashRateBuying");
+                    Double JPYspotRateBuying = jsonObject7.getDouble("spotRateBuying");
+                    JPYcash =JPYcashRateBuying;
+                    JPYspot =JPYspotRateBuying;
+
+                    ////////歐元
+                    JSONObject jsonObject14 = array.getJSONObject(14);
+                    Double EURcashRateBuying = jsonObject14.getDouble("cashRateBuying");
+                    Double EURspotRateBuying = jsonObject14.getDouble("spotRateBuying");
+                    EURcash =EURcashRateBuying;
+                    EURspot =EURspotRateBuying;
+
+                    ////////人民幣
+                    JSONObject jsonObject18 = array.getJSONObject(18);
+                    Double CNYcashRateBuying = jsonObject18.getDouble("cashRateBuying");
+                    Double CNYspotRateBuying = jsonObject18.getDouble("spotRateBuying");
+                    CNYcash =CNYcashRateBuying;
+                    CNYspot =CNYspotRateBuying;
+
+                    ////////澳幣
+                    JSONObject jsonObject3 = array.getJSONObject(3);
+                    Double AUDcashRateBuying = jsonObject3.getDouble("cashRateBuying");
+                    Double AUDspotRateBuying = jsonObject3.getDouble("spotRateBuying");
+                    AUDcash =AUDcashRateBuying;
+                    AUDspot =AUDspotRateBuying;
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
     }
 
 }
